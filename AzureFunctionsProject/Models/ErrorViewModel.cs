45d23c4ddd21f890c0a3597db26adb0cc23d9c7e
Suman Chava <Suman.Chava@blueyonder.com>
2024-06-27 20:13:09 +0530

    Added project for Functions Demo

---
+++ AzureFunctionsProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureFunctionsProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
